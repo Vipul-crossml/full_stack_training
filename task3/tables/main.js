9aaa8d5f9ce122aaa87c67d6831455140d899e8e
Vipul-crossml <vipul@crossml.com>
2022-06-14 13:15:03 +0530

    new changes

--- task3/tables/main.js
+++ task3/tables/main.js
@@ -1,10 +1,10 @@
 // script 
-
+// task 1
 console.log("hello")
 
 
 var checkb = document.querySelectorAll('.checkbox1');
-console.log(checkb)
+
 
 
 sum1 = 0;
@@ -67,4 +67,45 @@ function first() {
     credit = sum2 - sum4
     document.getElementById('selected').innerHTML = debit;
     document.getElementById('selected2').innerHTML = credit;
+}
+
+// end task 1
+
+
+// task 2 
+count = 0;
+const count_list = []
+function newRow() {
+    a = count++;
+    count_list.push(a);
+    DEBIT = document.createElement('input');
+    DEBIT.setAttribute("type", 'text');
+    DEBIT.setAttribute("class", 'text');
+    DEBIT.setAttribute("id", +count + 'debit');
+    DEBIT.setAttribute("onkeypress", 'myFunction1(event)');
+
+
+    CREDIT = document.createElement('input');
+    CREDIT.setAttribute("type", 'text');
+    CREDIT.setAttribute("id", +count + 'credit');
+    CREDIT.setAttribute("onkeypress", 'myFunction2(event)');
+
+
+    var slect = document.getElementById("add-row");
+    option = slect.options[slect.selectedIndex].value;
+
+    var tbodyRef = document.getElementById('table3');
+
+    // Insert a row at the end of table
+    var newRow = tbodyRef.insertRow(1);
+
+    // Insert a cell at the end of the row
+    var newCell = newRow.insertCell(0);
+    var newCell1 = newRow.insertCell(1).appendChild(DEBIT);
+    var newCell2 = newRow.insertCell(2).appendChild(CREDIT);
+    var newCell3 = newRow.insertCell(3);
+    var newCell4 = newRow.insertCell(4);
+
+    newCell.innerHTML = option;
+
 }
