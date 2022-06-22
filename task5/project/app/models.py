935e6e7d1e79eaa3272998c4c4f60ad19c2a3838
Vipul-crossml <vipul@crossml.com>
2022-06-22 10:11:08 +0530

    new changes

--- task5/project/app/models.py
+++ task5/project/app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+
+pipeline_options = (
+    (0,'Engine 1'),
+    (1,'Engine 2')
+)
+
+classification_model_options = (
+    (0,'Engine 1'),
+    (1,'Engine 2')
+)
+
+# Create your models here.
+class NewProcess(models.Model):
+    name = models.CharField(max_length=20)
+    classication = models.CharField(choices=classification_model_options)
+    pipeline = models.CharField(choices=pipeline_options)
