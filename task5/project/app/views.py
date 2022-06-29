f8a4961e24b1b5532efec4b30dee6c19f1348e48
Vipul-crossml <vipul@crossml.com>
2022-06-29 15:19:03 +0530

    new changes

--- task5/project/app/views.py
+++ task5/project/app/views.py
@@ -1,24 +1,30 @@
 from django.http import HttpResponse
 from django.shortcuts import redirect, render
 from .forms import *
+from django.contrib.auth.models import User
 # Create your views here.
 
 
 def index(request):
     form = ConfigureNewProcessForm()
-    return render(request, "main.html",context={'form':form})
-
+    return render(request, "main.html", context={'form': form})
 
 
 def add_new_process(request):
-    form=ConfigureNewProcessForm()
-    print(form)
+    form = ConfigureNewProcessForm()
     if request.method == "POST":
-        form = ConfigureNewProcessForm(request.POST)
-        print(form.is_valid())
-        if form.is_valid():
-            form.save()
-        return redirect(index)
+        firstname = request.POST.get('firstname')
+        lastname = request.POST.get('lastname')
+        username = request.POST.get('email')
+        password = request.POST.get('password')
+        confirmpassword = request.POST.get('confirmpassword')
+        if password==confirmpassword:
+            user = User.objects.create_user(first_name=firstname, last_name=lastname,username=username,password=password)
+            form = ConfigureNewProcessForm(request.POST)
+            print(form.is_valid())
+            if form.is_valid():
+                form.save()
+            return redirect(cnn)
 
 def cnn(request):
     form = ProcessManagementForm()
