f6d95db0ce76017ad49f8860c7de583dc74b3244
Vipul-crossml <vipul@crossml.com>
2022-07-14 20:05:50 +0530

    deleted previous project

--- task5/project/app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('add_new_process', views.add_new_process, name='add_new_process'),
-    path('cnn', views.cnn, name='cnn'),
-    path('attribute', views.attribute, name='attribute'),
-]
